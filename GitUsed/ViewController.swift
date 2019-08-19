884b6da6a4d063f21264d12a839939432d1ffe26
Shiv Kumar <sivu10196@gmail.com>
2019-08-19 16:17:58 +0530

    inicial changes

---
+++ GitUsed/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitUsed
+//
+//  Created by Shiv kumar on 8/19/19.
+//  Copyright © 2019 Shiv kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("ViewDidLoad")
+    }
+
+
+}
+
